b258e84ae57ea0ccc4c50bf5eebcfdae6372cef9
gesslar <1266935+gesslar@users.noreply.github.com>
2026-03-15 11:34:42 -0400

    nvm

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -82,12 +82,6 @@ const config = {
           label: 'mpackage',
         },
         {
-          type: 'docSidebar',
-          sidebarId: 'chromeManifest',
-          position: 'left',
-          label: 'Chrome Manifest',
-        },
-        {
           href: 'https://github.com/gesslar/schema.gesslar.dev',
           label: 'GitHub',
           position: 'right',
